3fe80c3f1c8f073582386f706df2f184b477fe50
Kerjox <kerjoxgamer095@gmail.com>
2022-12-13 21:19:51 +0100

    :sparkles: Añadido temario 01. ITN

---
+++ Clean_Temario.py
@@ -0,0 +1,11 @@
+# Remove files thar has extension .descarga
+
+import os
+import sys
+
+path = sys.argv[1]
+for root, dirs, files in os.walk(path):
+    for file in files:
+        if file.endswith('.descarga'):
+            print('Removing: ' + os.path.join(root, file))
+            os.remove(os.path.join(root, file))
